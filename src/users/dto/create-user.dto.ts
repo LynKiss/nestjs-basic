35e991097fe7ae8f83bccc4c035dd2265c656d60
Lyn <lyn250904@gmail.com>
2026-04-09 13:38:35 +0700

    build lại schema

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,24 +1,100 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { Type } from 'class-transformer';
+import {
+  IsEmail,
+  IsMongoId,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+
+class Company {
+  @IsMongoId({
+    message: 'Company id khong dung dinh dang',
+  })
+  @IsNotEmpty({
+    message: 'Company id khong duoc de trong',
+  })
+  _id: string;
+
+  @IsString()
+  @IsNotEmpty({
+    message: 'Ten cong ty khong duoc de trong',
+  })
+  name: string;
+}
 
 export class CreateUserDto {
+  @IsNotEmpty({
+    message: 'Name khong duoc de trong',
+  })
+  @IsString()
+  name: string;
+
   @IsEmail(
     {},
     {
-      message: 'Title is too short',
+      message: 'Email khong dung dinh dang',
     },
   )
   @IsNotEmpty({
-    message: 'Title is too short',
+    message: 'Email khong duoc de trong',
   })
   email: string;
 
   @IsNotEmpty({
-    message: 'Title is too short',
+    message: 'Password khong duoc de trong',
   })
+  @IsString()
   password: string;
 
+  @IsNumber()
+  @IsNotEmpty({
+    message: 'Age khong duoc de trong',
+  })
+  age: number;
+
+  @IsNotEmpty({
+    message: 'Gender khong duoc de trong',
+  })
+  @IsString()
+  gender: string;
+
+  @IsNotEmpty({
+    message: 'Address khong duoc de trong',
+  })
+  @IsString()
+  address: string;
+
+  @IsNotEmpty({
+    message: 'Company khong duoc de trong',
+  })
+  @ValidateNested()
+  @Type(() => Company)
+  company: Company;
+}
+
+export class RegisterUserDto {
+  @IsNotEmpty({
+    message: 'Name khong duoc de trong',
+  })
+  @IsString()
   name: string;
-  phone?: string;
-  age?: number;
-  address?: string;
+
+  @IsEmail(
+    {},
+    {
+      message: 'Email khong dung dinh dang',
+    },
+  )
+  @IsNotEmpty({
+    message: 'Email khong duoc de trong',
+  })
+  email: string;
+
+  @IsNotEmpty({
+    message: 'Password khong duoc de trong',
+  })
+  @IsString()
+  password: string;
 }
--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,13 +1,11 @@
-import { Company } from './../../companys/schemas/company.schema';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { timeStamp } from 'console';
-import { HydratedDocument } from 'mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
 
 export type UserDocument = HydratedDocument<User>;
 
 @Schema({ timestamps: true })
 export class User {
-  @Prop({ required: true })
+  @Prop({ required: true, unique: true })
   email: string;
 
   @Prop({ required: true })
@@ -17,27 +15,75 @@ export class User {
   name: string;
 
   @Prop()
-  company: string;
-
-  @Prop()
   age: number;
+
   @Prop()
   gender: string;
 
   @Prop()
   address: string;
 
-  @Prop()
-  createAt: Date;
+  @Prop({
+    type: {
+      _id: { type: mongoose.Schema.Types.ObjectId, ref: 'Company' },
+      name: { type: String },
+    },
+  })
+  company: {
+    _id: mongoose.Schema.Types.ObjectId;
+    name: string;
+  };
 
   @Prop()
-  isDeletedAt: boolean;
+  role: string;
+
+  @Prop()
+  refreshToken: string;
+
+  @Prop({
+    type: {
+      _id: { type: mongoose.Schema.Types.ObjectId },
+      email: { type: String },
+    },
+  })
+  createdBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+
+  @Prop({
+    type: {
+      _id: { type: mongoose.Schema.Types.ObjectId },
+      email: { type: String },
+    },
+  })
+  updatedBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+
+  @Prop({
+    type: {
+      _id: { type: mongoose.Schema.Types.ObjectId },
+      email: { type: String },
+    },
+  })
+  deletedBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+
+  @Prop()
+  createdAt: Date;
+
+  @Prop()
+  updatedAt: Date;
+
+  @Prop()
+  isDeleted: boolean;
 
   @Prop()
   deletedAt: Date;
-
-  @Prop()
-  updateAt: Date;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
